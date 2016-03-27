78facb229f30d98691354c23c94423c1fca246fc
Jose Aviles <jlapmail@gmail.com>
2016-03-27 15:08:48 -0400

    Fixed user field 'name'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,12 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
+protected
 
   def configure_permitted_parameters
 
-      permitted_params = [:email, :password, :password_confirmation, :unit_prefs, :name]
+  devise_parameter_sanitizer.for(:sign_up) << :name
+  devise_parameter_sanitizer.for(:account_update) << :name  
 
   end
 end
